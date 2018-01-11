72ff6d7e943a1607710b85b0c1e314657d3d9564
shanxuan <781244184@qq.com>
2018-01-11 19:05:32 +0800

    加上部分注释

--- example.py
+++ example.py
@@ -1,9 +1,6 @@
-from pyaria2 import Jsonrpc
 
-host = 'localhost'
-port = 6800
-jsonrpc = Jsonrpc(host, port)
+from pyaria2 import Jsonrpc
+jsonrpc = Jsonrpc('localhost', 6800)
 resp = jsonrpc.addUris('http://182.131.21.97:80/resource/music/music/jianggaranzuhe/320_jianggaranzuhelangsangguniang.mp3', options={"out": "aa.mp3"})
 print resp
-
 
--- hook.py
+++ hook.py
@@ -7,6 +7,8 @@ This implements the --on-download-complete hook interface.
 
 import sys
 
-print "sys's argv:"
-print sys.argv
+# ['/Users/csx/GitProject/snowmusic/pyaria2-jsonrpc/hook.py', 'e3f97be6d4490a5a', '1', './temp/aa.mp3']
+Argv = sys.argv
 
+def Hook(Argv):
+    print Argv
